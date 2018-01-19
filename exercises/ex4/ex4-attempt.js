a9b07cc44dbf4ec527096a5905e98857adc9c073
TheBrooks <the.ryan.brooks@gmail.com>
2018-01-19 09:33:57 -0700

    exercise 4 attempted

---
+++ exercises/ex4/ex4-attempt.js
@@ -0,0 +1,19 @@
+function upperCaseValues(strings,...values) {
+  var str = '';
+  for (let i = 0; i < strings.length; i++){
+    if (i > 0) {
+      str += values[i-1].toUpperCase();
+    }
+    str += strings[i]
+  }
+  return str;
+}
+
+var name = "kyle",
+	twitter = "getify",
+	classname = "es6 workshop";
+
+console.log(
+	upperCaseValues`Hello ${name} (@${twitter}), welcome to the ${classname}!` ===
+	"Hello KYLE (@GETIFY), welcome to the ES6 WORKSHOP!"
+);
